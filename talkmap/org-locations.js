cad71b6ae5116753fe5dda21ea87f6e0516576be
Salazar Luces Jose <jose@irs.mech.tohoku.ac.jp>
2020-08-26 21:58:46 +0900

    Added most talks, generated map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,17 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
+    "Munich, Germany", 
+    48.1371079, 
+    11.5753822
+  ], 
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
+    "Honolulu, Hawaii", 
+    21.304547, 
+    -157.8556764
+  ], 
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Daejon, Korea", 
+    36.3496, 
+    127.3848
   ]
 ];
